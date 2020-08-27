489a7cfab2e70fcc59d7afddaf892ec52db83ee4
westerntrojan <westerntrojan@gmail.com>
2020-08-27 12:25:34 +0500

    added lazy loading images

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import './Image.scss';
 
 const Image = ({ src, alt, onClick }) => (
-  <span className="image" onClick={onClick}>
-    <img src={src} alt={alt} />
+  <span className="image-wrapper" onClick={onClick}>
+    <LazyLoadImage
+      src={src}
+      alt={alt}
+      width={'100%'}
+      height={'100%'}
+      effect="blur"
+      style={{ display: 'flex', objectFit: 'cover' }}
+    />
   </span>
 );
 
